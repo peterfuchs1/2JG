10587addce872eeaaee745b68e79830e3f623cb1
peterfuchs1 <peterfuchs_1@yahoo.de>
2013-11-28 09:28:51 +0100

    Panel mit Label

--- 2JG/src/awt/MyFirstPanel.java
+++ 2JG/src/awt/MyFirstPanel.java
@@ -3,6 +3,7 @@
  */
 package awt;
 
+import java.awt.Color;
 import java.awt.Label;
 import java.awt.Panel;
 
@@ -11,14 +12,16 @@ import java.awt.Panel;
  *
  */
 public class MyFirstPanel extends Panel {
-	private Label l;
+	private Label l;		//Label zur Darstellung des Textes
 
 	/**
 	 * Konstruktor
-	 * @param String 
+	 * @param t Text, der im Label dargestellt werden soll 
 	 */
 	public MyFirstPanel(String t) {
 		l=new Label(t);		// Ein neues Label mit Text wird erstellt
+		l.setForeground(Color.BLACK);	// Farbe für Schrift gesetzt
+		l.setBackground(Color.WHITE);	// Farbe für Hintergrund gesetzt
 		this.add(l);		// Das Label wird dem Panel hinzugefügt
-	}	
+	}
 }
