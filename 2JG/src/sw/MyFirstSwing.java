b8fe051ea49b2f16836072350ff4f99dbac6d35a
HIT Schueler <hit.schueler@gmx.at>
2013-12-13 11:39:38 +0100

    changed to Builder

--- 2JG/src/sw/MyFirstSwing.java
+++ 2JG/src/sw/MyFirstSwing.java
@@ -1,21 +1,22 @@
 package sw;
 
 import java.awt.BorderLayout;
-import java.awt.EventQueue;
-
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
-import javax.swing.JButton;
-import javax.swing.JTextField;
-import javax.swing.JLabel;
 import java.awt.Color;
-import javax.swing.SwingConstants;
-import java.awt.event.ActionListener;
-import java.awt.event.ActionEvent;
+import java.awt.EventQueue;
 import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.SwingConstants;
+import javax.swing.border.EmptyBorder;
 
 public class MyFirstSwing extends JFrame {
+
 
 	private JPanel contentPane;
 	private JTextField jtfName;
