a2f001271ed81aa71552d92a98e30bf932558142
HIT Schueler <hit.schueler@gmx.at>
2013-12-15 12:49:35 +0100

    Beispiel: Summe mit Button

---
+++ 2JG/src/sw/MySecondJPanel.java
@@ -0,0 +1,86 @@
+package sw;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.GridBagLayout;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+/**
+ * Einfacher JPanel mit ActionListener
+ * @author Walter Rafeiner-Magor
+ *
+ */
+public class MySecondJPanel extends JPanel implements ActionListener{
+	private JTextField jtfa,jtfb;	// Eingabefelder
+	private JButton jb;				// Button zur Berechnung
+	private JLabel jle;				// Ergebnisfeld
+	/**
+	 * JPanel mit Button
+	 * @param t
+	 */
+	public MySecondJPanel(String t) {
+		this.setLayout(new BorderLayout(0,5));
+		this.setBackground(new Color(240, 230, 140));
+		// Im Norden
+		JPanel north=new JPanel();
+		north.setBackground(null); //JPanel ist durchsichtig
+		JLabel jlt=new JLabel(t);
+		north.add(jlt);
+		this.add(north,BorderLayout.NORTH);
+		
+		//Im Center
+		JPanel center=new JPanel();
+		center.setBackground(null); //JPanel ist durchsichtig
+		// GridLayout mit 2 Zeilen und 5 Spalten 
+		center.setLayout(new GridLayout(2,5,10,10));
+		jtfa=new JTextField(4);
+		jtfa.addActionListener(this); // unsere Klasse kümmert sich um ENTER
+		jtfb=new JTextField(4);
+		jtfb.addActionListener(this); // unsere Klasse kümmert sich um ENTER
+		// Labels erstellen
+		JLabel jla=new JLabel("A");
+		JLabel jlb=new JLabel("B");
+		JLabel jlp=new JLabel(" + ");
+		JLabel jlg=new JLabel(" = ");
+		jle=new JLabel("");
+		// Elemente im GridLayout verankern
+		// Erste Zeile
+		center.add(jla);			// A
+		center.add(new JLabel()); //Dummy
+		center.add(jlb);			// B
+		center.add(new JLabel()); //Dummy
+		center.add(new JLabel("Ergebnis"));
+		// Zweite Zeile
+		center.add(jtfa);	// Eingabefeld für A
+		center.add(jlp);	// +
+		center.add(jtfb);	// Eingabefeld für B
+		center.add(jlg);	// =
+		center.add(jle);	// Ergebnisfeld
+		this.add(center, BorderLayout.CENTER);
+		
+		// Im Süden
+		JPanel south=new JPanel();
+		south.setBackground(null); //JPanel ist durchsichtig
+		jb=new JButton("Berechnen");
+		jb.addActionListener(this); // unsere Klasse kümmert sich um ENTER
+		south.add(jb);
+		this.add(south, BorderLayout.SOUTH);
+	}
+	/**
+	 * Falls Enter oder Button gedrückt wurde
+	 */
+	@Override
+	public void actionPerformed(ActionEvent ae) {
+		// Werte auslesen und in double-Werte umwandeln
+		double a=Double.parseDouble(jtfa.getText());
+		double b=Double.parseDouble(jtfb.getText());
+		jle.setText(""+(a+b));	// Ergebnis als String setzen
+	}	
+}
---
+++ 2JG/src/sw/MySecondSwingFrame.java
@@ -0,0 +1,30 @@
+package sw;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+/**
+ * Gui mittels Swing-Komponenten
+ * @author Walter Rafeiner-Magor
+ * @version 1.1
+ */
+public class MySecondSwingFrame extends JFrame{
+	public MySecondSwingFrame() {
+		// Einen JPanel erzeugen
+		MySecondJPanel jp= new MySecondJPanel("Bitte für A und B reele Werte eingeben");
+		// und im CENTER des JFrames verankern
+		this.add(jp,BorderLayout.CENTER);
+		this.setSize(320, 160);			// Größe 320x160 Pixel
+		this.setResizable(false);		// Größe nicht veränderbar
+		this.setLocationRelativeTo(null);	// Darstellung mittig am Bildschirm
+		// Standardmethode beim Schließen des Fensters
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setVisible(true);		// JFrame ist nun sichtbar
+	}
+	public static void main(String[] args) {
+		new MySecondSwingFrame();
+	}
+}
