886d1321d597850c1e965035f5110c8990d25675
HIT Schueler <hit.schueler@gmx.at>
2014-03-18 08:28:10 +0100

    Für Lisa etwas aufgeraeumt ;-)

--- 2JG/src/paint/Zeichenbrett.java
+++ 2JG/src/paint/Zeichenbrett.java
@@ -2,7 +2,7 @@ package paint;
 /**
  * Startklasse des Zeichenbretts
  * @author Walter Rafeiner-Magor
- * @version 1.8
+ * @version 3.0
  */
 public class Zeichenbrett {
 	public static void main(String[] args) {
